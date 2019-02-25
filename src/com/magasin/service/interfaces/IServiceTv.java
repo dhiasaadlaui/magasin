3d51b0586fcf5cd796267b25f629ca2ccc77fb68
Saadlaui Mohamed Dhia <mohameddhia.saadlaui@esprit.tn>
2019-02-25 11:18:41 +0100

    ServiceTv search field fixed

--- src/com/magasin/service/interfaces/IServiceTv.java
+++ src/com/magasin/service/interfaces/IServiceTv.java
@@ -5,5 +5,8 @@ import java.util.List;
 import com.magasin.entities.Tv;
 
 public interface IServiceTv extends IGenericService<Tv> {
-	List<Tv> search ( Object  field,Object value  );
+	public enum TvSearchFields {
+		RESOLUTION;
+	}
+	List<Tv> search ( TvSearchFields  field,Object value  );
 }
