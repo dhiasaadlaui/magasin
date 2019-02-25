5d9bb335e8522f94706c2cfa975b92ea18ec1fc5
Saadlaui Mohamed Dhia <mohameddhia.saadlaui@esprit.tn>
2019-02-25 09:10:06 +0100

    Main Menu done, StocGui  Done, ServicePull Done

--- src/com/magasin/gui/StockGUI.java
+++ src/com/magasin/gui/StockGUI.java
@@ -1,9 +1,90 @@
 package com.magasin.gui;
 
+import java.util.Scanner;
+ 
+
 public class StockGUI {
+	private Scanner scn;
 
 	public StockGUI() {
+		
 		// TODO Auto-generated constructor stub
+		boolean test=true;
+	 do {
+		top();
+		System.out.println("____________________________________________________________________________");
+		frame(1);
+		System.out.println("|====================== Welcome To Stock Management =======================|");
+		horizental();
+		frame(4);
+		System.out.println("|                  +++++ What Operation to do ? +++++                      |");
+		frame(3);
+		System.out.println("|  1 > Add New Article.                                                    |");
+		frame(1);
+		System.out.println("|  2 > Search For Article.                                                 |");
+     	frame(1);
+		System.out.println("|  3 > Back.                                                               |");
+		horizental();
+		
+		scn = new Scanner(System.in);
+		String x = scn.next();
+		//test on the entry of the user
+		switch(x)
+		{ 
+		case "1":
+			for (int i = 0; i < 50; ++i) System.out.println();
+			new AddArticleGUI();
+			break;
+		case "2":
+			for (int i = 0; i < 50; ++i) System.out.println();
+			new SearchArticleGUI();
+			break;
+		case"3":  
+			for (int i = 0; i < 50; ++i) System.out.println(); 
+			MainMenu.main(null);
+		 
+		}
+	 }while(test);
+		
+		
+		
+
 	}
+ 
+
+	
+	public static void top()
+	{
+		for(int i=0;i<36;i=i+3)
+		{
+			
+			for(int j=i;j<36;j++)
+			{
+				System.out.print(" ");
+			}
+			System.out.print("/");
+			for(int j=0;j<i*2;j++)
+			{
+				System.out.print(" ");
+			}
+			System.out.println(" \\ ");
+		}	
+	}
+	public static void frame(int length)
+	{
+		for(int i=0;i<length;i++)
+		{
+			System.out.println("|                                                                          |");
+		}
+	}
+	
+	public static void horizental()
+	{
+		
+			System.out.println("|__________________________________________________________________________|");
+		 
+	}
+	
+	
 
 }
--- src/com/magasin/service/implementation/ServicePullImpl.java
+++ src/com/magasin/service/implementation/ServicePullImpl.java
@@ -2,48 +2,55 @@ package com.magasin.service.implementation;
 
 import java.util.List;
 
+import com.magasin.dao.implementation.PullDaoImpl;
+import com.magasin.dao.interfaces.IPullDao;
 import com.magasin.entities.Pull;
 import com.magasin.service.interfaces.IServicePull;
 
 public class ServicePullImpl implements IServicePull {
 
+	private IPullDao pulldo;
 	public ServicePullImpl() {
 		// TODO Auto-generated constructor stub
+		pulldo = new PullDaoImpl();
 	}
 
 	@Override
 	public Pull findById(int id) {
 		// TODO Auto-generated method stub
-		return null;
+		
+		return pulldo.findById(id);
 	}
 
 	@Override
 	public List<Pull> findAll() {
 		// TODO Auto-generated method stub
-		return null;
+		return pulldo.findAll();
 	}
 
 	@Override
 	public int create(Pull entite) {
 		// TODO Auto-generated method stub
-		return 0;
+		return pulldo.create(entite);
 	}
 
 	@Override
 	public int edit(Pull entite) {
 		// TODO Auto-generated method stub
-		return 0;
+		return pulldo.edit(entite);
 	}
 
 	@Override
 	public int delete(Pull entite) {
 		// TODO Auto-generated method stub
-		return 0;
+		return pulldo.delete(entite);
 	}
 
 	@Override
 	public List<Pull> search(Object field, Object value) {
 		// TODO Auto-generated method stub
+		if(field.equals("taille"))
+			return pulldo.rechercherParTaille((String)value);
 		return null;
 	}
 
