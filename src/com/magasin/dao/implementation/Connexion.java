4b8855d76ab0e5726e0ce4bc0343f577d96e9dd6
Saadlaui Mohamed Dhia <mohameddhia.saadlaui@esprit.tn>
2019-02-25 08:48:29 +0100

    fixin connection

--- src/com/magasin/dao/implementation/Connexion.java
+++ src/com/magasin/dao/implementation/Connexion.java
@@ -1,51 +1,75 @@
 package com.magasin.dao.implementation;
 
+import java.io.FileInputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
-
+import java.util.Properties;
+import java.util.logging.Logger;
+/*
+ * 
+ */
 public class Connexion {
-	private String url = "jdbc:mysql://localhost:3306/magasin";
-	private String username = "root";
-	private String password = "";
+	
+	private static final Logger LOGGER = Logger.getLogger(Connexion.class.getName());
+	private String url;
+	private String username;
+	private String password;
+	private String driver;
 	private static Connexion instance = null;
 	private Connection cn;
-
 	public static Connexion getInstance() {
 		if (instance == null) {
 			synchronized (Connexion.class) {
-				if (instance == null) {
+				if (instance == null)
 					instance = new Connexion();
-					System.out.println("Connected !!!!!!!!!!!!!!!!");
-				}
 			}
 
 		}
-//		else
-//		{
-//			System.out.println("already connected !!!!!!!!!!!!!!!!");
-//		}
-//		
 
 		return instance;
 	}
 
 	private Connexion() {
+		try {
 
+			FileInputStream fis = new FileInputStream(".\\src\\connection.properties");
+			Properties p = new Properties();
+			p.load(fis);
+			url = (String) p.get("url");
+			username = (String) p.get("user");
+			password = (String) p.get("password");
+			driver = (String) p.get("driver");
+			LOGGER.info("The class ("+LOGGER.getName()+") attempting to connect database ..");
+			
+		} catch (Exception e) {
+			e.printStackTrace();// TODO: handle exception
+		}
 	}
 
 	public Connection connect() {
 		try {
-			Class.forName("com.mysql.jdbc.Driver");
+			Class.forName(driver);
 			
 			cn = DriverManager.getConnection(url, username, password);
-			// System.out.println("Connected");
+			
 		} catch (ClassNotFoundException | SQLException e) {
 			e.printStackTrace();
+			LOGGER.warning("The class ("+LOGGER.getName()+") failed to connected  database ..");			
+			
 		}
 		return cn;
 	}
 
+	
+	public ResultSet getResult(String query) throws SQLException {
+		
+		return getInstance().connect().createStatement().executeQuery(query);
+	}
+	
+	
+	
 	public void disconnect() {
 		try {
 			if (cn != null)
