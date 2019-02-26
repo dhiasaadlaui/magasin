a8a8e99aaaeba08673739aee9e7b5d94b09605ba
Saadlaui Mohamed Dhia <mohameddhia.saadlaui@esprit.tn>
2019-02-26 09:20:48 +0100

    ArticleDAO fixing code format and visibility

--- src/com/magasin/gui/SearchArticleGUI.java
+++ src/com/magasin/gui/SearchArticleGUI.java
@@ -1,9 +1,12 @@
 package com.magasin.gui;
 
+import com.magasin.entities.Article;
+
 public class SearchArticleGUI {
 
 	public SearchArticleGUI() {
 		// TODO Auto-generated constructor stub
+		new DeleteArticleGUI(new Article());
 	}
 
 }
